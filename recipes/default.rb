4bcb6a410a0a55709feffd72a1ea30a99793799b
Michael Gutteridge <mrg@fhcrc.org>
2016-05-05 12:29:19 -0700

    added template for knife configuration

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,9 +25,21 @@ gem_package 'knife-supermarket' do
 end
 
 user 'btb' do
+    supports :manage_home => true
     comment 'Bob the Builder Automation User'
     home '/var/spool/btb'
     shell '/bin/bash'
+end
+
+directory '/var/spool/btb/.chef' do
+    owner 'btb'
+    mode '0700'
+end
+
+template '/var/spool/btb/.chef/knife.rb' do
+    source 'knife.rb.erb'
+    owner 'btb'
+    mode '0644'
 end
 
 
