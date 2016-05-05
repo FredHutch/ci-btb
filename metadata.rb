6fe8a26b377af83641898088cf81796872a3b172
Michael Gutteridge <mrg@fhcrc.org>
2016-05-05 16:44:03 -0700

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures ci-btb'
 long_description 'Installs/Configures ci-btb'
-version '0.1.2'
+version '0.1.3'
 
 depends 'apt'
 depends 'apt-chef'
 depends 'jenkins', '~> 2.4.1'
-depends 'nginx',  '~> 2.7.7'
+depends 'nginx',  '~> 2.7.8'
 
