c364c0059753140b49067fd8fdb677b9c1c90055
Michael Gutteridge <mrg@fhcrc.org>
2016-05-05 11:45:06 -0700

    swapped out packagecloud for Chef.io repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 include_recipe 'apt::default'
+include_recipe 'apt-chef::stable'
 
 apt_repository 'octopus_fhcrc' do
     uri             'http://octopus.fhcrc.org/fhcrc/ubuntu'
@@ -15,10 +16,6 @@ apt_repository 'octopus_fhcrc' do
     deb_src         false
 end
 
-packagecloud_repo "chef/stable" do
-    type "deb"
-end
-
 package [ 'chefdk' ] do
     action :install
 end
@@ -27,8 +24,15 @@ gem_package 'knife-supermarket' do
     gem_binary '/opt/chefdk/embedded/bin/gem'
 end
 
-execute 'add local supermarket certs' do
-    command "knife ssl fetch #{node['supermarket_uri']}"
-    not_if "knife ssl check #{node['supermarket_uri']}"
+user 'btb' do
+    comment 'Bob the Builder Automation User'
+    home '/var/spool/btb'
+    shell '/bin/bash'
 end
+
+
+#execute 'add local supermarket certs' do
+#    command "knife ssl fetch #{node['supermarket_uri']}"
+#    not_if "knife ssl check #{node['supermarket_uri']}"
+#end
 
