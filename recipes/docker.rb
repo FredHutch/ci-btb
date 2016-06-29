54ee026312f87037b99ec5c21f76574f52b54a30
Michael Gutteridge <mrg@fhcrc.org>
2016-06-29 13:27:30 -0700

    pulled out docker registry code

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -6,9 +6,6 @@
 #
 # Configure docker on the host for test kitchen use
 
-include_recipe 'chef-vault'
-config = chef_vault_item('docker', 'config')
-
 include_recipe 'chef-apt-docker'
 
 docker_installation_package 'default' do
@@ -34,8 +31,3 @@ gem_package 'kitchen-docker' do
   options('--no-user-install')
 end
 
-docker_registry config['registry_url'] do
-  username config['username']
-  password config['password']
-  email config['email']
-end
